2164b60ff4d531c76fd0a46f459e97281e7af4ff
Davidmarga <davidmargalejorgmail.com>
2025-08-20 18:10:08 +0200

    prueba

--- API_SENCILLA/controlador.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/modelo.php';
-
-class UsuarioController {
-    public static function listarUsuarios() {
-        return Usuario::obtenerTodos();
-    }
-}
-?>
--- API_SENCILLA/servicio.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json");
-
-require_once __DIR__ . '/controlador.php';
-
-$usuarios = UsuarioController::listarUsuarios();
-echo json_encode($usuarios);
-?>
